340e1a98f3bdc3cbbc86bd0bc554e512136f2cd2
Russell <russellgoldenberg@gmail.com>
2012-06-20 18:14:20 -0400

    db connection works

--- environment.js
+++ environment.js
@@ -1,18 +1,34 @@
+var mongoose = require('mongoose');
 module.exports.appName = "Express template",
-module.exports.db = {
-	URL: 'mongodb://localhost:27017/template_development'
-}
+// module.exports.db = {
+// 	URL: 'mongodb://localhost:27017/template_development'
+// }
+
+
 
 // // Database (WE SHOULD FIGURE OUT DEV VERSUS STAGING VERSUS LIVE)
 // mongoose.connect('mongodb://localhost/civic_dev_db');
 // var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
-// mongoose.connect('mongodb://root:root@ds033767.mongolab.com:33767/civicseeddev');
-// var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
+mongoose.connect('mongodb://root:root@ds033767.mongolab.com:33767/civicseeddev');
+var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
-// var Person = new Schema({
-// 	author: ObjectId,
-// 	title: String,
-// 	body: String,
-// 	date: Date
+var User = new Schema({
+	username: String,
+	title: String
+});
+
+
+
+var userModel = mongoose.model('User', User);
+// var user = new userModel();
+
+// user.username = 'Grumpy';
+// user.title = 'Old Man Coder';
+// user.save(function(err) {
+// 	if (err) throw err;
+// 	console.log('User saved! Hooray!');
 // });
+var all = userModel.find(function(err,docs){
+	console.log(docs);
+});
