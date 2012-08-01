fb7bd88e173e1d2f2843585ee01dce1660db5b6f
American Yak <langbert@gmail.com>
2012-07-31 21:29:04 -0400

    Express and SocketStream joined at the hip.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-var server,
-http = require('http'),
-ss = require('socketstream'),
+var ss = require('socketstream'),
 express = require('express'),
 app = module.exports = express(),
-environment = require('./environment.js'),
-service = require('./service.js'),
-config = require('./configuration.js')(app, express, ss),
+server,
+environment,
+service,
+config,
+control;
+
+console.log('\n\n   * * * * * * * * * * * *   Starting the Civic Seed Game Engine   * * * * * * * * * * * *   \n\n'.yellow)
+
+// Append SocketStream middleware to the stack
+app.stack = app.stack.concat(ss.http.middleware.stack);
+
+// Configuration and environmental files, etc.
+environment = require('./environment.js');
+service = require('./service.js');
+config = require('./configuration.js')(app, express, ss);
 control = require('./controllers.js')(app, service, environment);
 
 // Setup the environment
@@ -14,7 +24,6 @@ service.init(environment);
 // // use redis
 // ss.session.store.use('redis');
 // ss.publish.transport.use('redis');
-
 
 // Define a single-page client
 ss.client.define('main', {
@@ -31,10 +40,20 @@ ss.client.define('main', {
 	tmpl: '*'
 });
 
-// Serve this client on the root URL
-ss.http.route('/game', function(req, res) {
-	res.serveClient('main');
-});
+// THESE ROUTES EVENTUALLY NEED TO GO INTO CONTROLLERS
+// BUT PLACING THEM HERE FOR NOW
+function routes(app) {
+
+	// express routes go like this
+	app.get('/', function (req, res) {
+		res.serve('main')
+	});
+
+	// socketstream routes go like this
+	ss.http.route('/game', function(req, res) {
+		res.serveClient('main');
+	});
+}
 
 // Minimize and pack assets if you type: SS_ENV=production node app.js
 if(ss.env == 'production') {
@@ -42,16 +61,10 @@ if(ss.env == 'production') {
 }
 
 // Start web server
-server = http.Server(ss.http.middleware);
-server.listen(3000, function() {
+server = app.listen(3000, function() {
 	var local = server.address();
 	console.log("Express server listening @ http://%s:%d/ in %s mode", local.address, local.port, app.settings.env);
 });
 
 // Start SocketStream
 ss.start(server);
-
-// server = app.listen(3000, function() {
-// 	var local = server.address();
-// 	console.log("Express server listening @ http://%s:%d/ in %s mode", local.address, local.port, app.settings.env);
-// });
