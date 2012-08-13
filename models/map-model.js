64ff13e2b5057934a1607af27529bb486514b744
American Yak <langbert@gmail.com>
2012-08-13 10:24:54 -0400

    Forgot to commit map model for data loading.

--- models/map-model.js
+++ models/map-model.js
@@ -1,80 +1,130 @@
-module.exports = function(mongoose) {
+module.exports = function(mongoose, db) {
 
-	// generated from tiles
-	// http://www.mapeditor.org/
-	var map = {
-		height: 40,
-		layers: [{
-			data: [
-				{
-					tile: 30,
-					color(rgba): transparent,
-					user: unassigned
-				},
-				{
-					tile: 30,
-					color(rgba): transparent,
-					user: unassigned
-				},
-				// etc.
-			],
-			height: 40,
-			name: "Ground",
-			opacity: 1,
-			type: "tilelayer",
-			visible: true,
-			width: 40,
-			x: 0,
-			y: 0
+	var Schema = mongoose.Schema;
+	var ObjectId = Schema.ObjectId;
+
+	// var MapSchema = new Schema({
+	// 	quadrant: [{
+	// 		neighbors: [Number, Number, Number, Number, Number, Number, Number, Number],
+	// 		tiles: [{
+	// 			localX: Number,
+	// 			localY: Number,
+	// 			worldX: Number,
+	// 			nogo: Boolean,
+	// 			worldY: Number
+	// 		}],
+	// 		quadrantNumber: Number,
+	// 		worldX: Number,
+	// 		worldY: Number
+	// 	}]
+	// });
+
+	var QuadrantSchema = new Schema({
+		neighbors: [Number, Number, Number, Number, Number, Number, Number, Number],
+		tiles: [{
+			localX: Number,
+			localY: Number,
+			worldX: Number,
+			nogo: Boolean,
+			worldY: Number
 		}],
-		orientation: orthogonal,
-		properties: {
+		quadrantNumber: Number,
+		worldX: Number,
+		worldY: Number
+	});
 
-		},
-		tileheight: 32,
-		tilesets: [{
-			firstgid: 1,
-			image: "..\/..\/..\/Volumes\/Tiled 0.8.1\/examples\/tmw_desert_spacing.png",
-			imageheight: 199,
-			imagewidth: 265,
-			margin: 1,
-			name: "Desert",
-			properties: {
-
-			},
-			spacing: 1,
-			tileheight: 32,
-			tilewidth: 32
-		}],
-		tilewidth: 32,
-		version: 1,
-		width: 40
-	};
-
-	var mapTwo = {
-
-		layers: {
-
-			grid: {
-
-			}
+	// var TileSchema = new Schema({
+	// 	localX: Number,
+	// 	localY: Number,
+	// 	worldX: Number,
+	// 	nogo: Boolean,
+	// 	worldY: Number
+	// });
 
 
-		}
+	//the third param specifies an exact collection to look for in the DB
+	// var MapModel = db.model('Map', MapSchema, 'maps');
+	var QuadrantModel = db.model('Quadrant', QuadrantSchema, 'quadrants');
+	// var TileModel = db.model('Tile', TileSchema, 'tiles');
 
 
 
-	}
+
+// 	// generated from tiles
+// 	// http://www.mapeditor.org/
+// 	var map = {
+// 		height: 40,
+// 		layers: [{
+// 			data: [
+// 				{
+// 					tile: 30,
+// 					color(rgba): transparent,
+// 					user: unassigned
+// 				},
+// 				{
+// 					tile: 30,
+// 					color(rgba): transparent,
+// 					user: unassigned
+// 				},
+// 				// etc.
+// 			],
+// 			height: 40,
+// 			name: "Ground",
+// 			opacity: 1,
+// 			type: "tilelayer",
+// 			visible: true,
+// 			width: 40,
+// 			x: 0,
+// 			y: 0
+// 		}],
+// 		orientation: orthogonal,
+// 		properties: {
+
+// 		},
+// 		tileheight: 32,
+// 		tilesets: [{
+// 			firstgid: 1,
+// 			image: "..\/..\/..\/Volumes\/Tiled 0.8.1\/examples\/tmw_desert_spacing.png",
+// 			imageheight: 199,
+// 			imagewidth: 265,
+// 			margin: 1,
+// 			name: "Desert",
+// 			properties: {
+
+// 			},
+// 			spacing: 1,
+// 			tileheight: 32,
+// 			tilewidth: 32
+// 		}],
+// 		tilewidth: 32,
+// 		version: 1,
+// 		width: 40
+// 	};
+
+// 	var mapTwo = {
+
+// 		layers: {
+
+// 			grid: {
+
+// 			}
 
 
-map of colors = {
-	color
-	colorvalue(rgba)
-	tied to a user
-	position
-}
+// 		}
 
 
-	return map;
+
+// 	}
+
+
+// map of colors = {
+// 	color
+// 	colorvalue(rgba)
+// 	tied to a user
+// 	position
+// }
+
+
+	return QuadrantModel;
 
 };
