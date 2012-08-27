6cda183573f48aa4c4f113de579f4ff8631a8f43
russell goldenberg <russellgoldenberg@gmail.com>
2012-08-27 15:59:40 -0400

    started planning out transition

--- client/code/game/controllers.js
+++ client/code/game/controllers.js
@@ -1,9 +1,30 @@
-// RUSS: do all your functions and members to "Game" in here
+ // shim layer with setTimeout fallback
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+
+var inTransit = false;
+
 var Game = {
+	
 	masterX: 0,
 	masterY: 0,
+	nextX: 0,
+	nextY: 0,
+	stepX: 0,
+	stepY: 0,
+	shiftArray: 0,
 	tilesWidth: 30,
 	tilesHeight: 15,
+	totalTilesWidth: 145,
+	totalTilesHeight: 140,
 	tileSize: 32,
 	tilesheet: null,
 	tilesheetWidth: 640/32,
@@ -13,197 +34,206 @@ var Game = {
 	foregroundContext: null,
 	charactersContext: null,
 	tilesheetContext: null,
-	tiles: [],
+	currentTiles: [],
+	nextTiles: [],
 
+	// Game initialization
 	init: function() {
-		// Game initialization
+		
+		//create offscreen canvas for the tilesheet
 		tilesheetCanvas = document.createElement('canvas');
-        // Set that canvas attributes
         tilesheetCanvas.setAttribute('width', Game.tilesheetWidth*Game.tileSize);
         tilesheetCanvas.setAttribute('height', Game.tilesheetHeight*Game.tileSize);
 
+        //initialize DB and let all players know there is a new active one
 		ss.rpc('multiplayer.init',function(response){
 			console.log("rpc init: "+response);
 		});
+
+		//load in tilesheet png
 		Game.tilesheet = new Image();
 		Game.tilesheet.src = 'img/game/tilesheet.png';
+		
+		//access the canvases for rendering
 		Game.backgroundContext = document.getElementById('background').getContext('2d');
 		Game.foregroundContext = document.getElementById('foreground').getContext('2d');
 		Game.charactersContext = document.getElementById('characters').getContext('2d');
 		Game.tilesheetContext = tilesheetCanvas.getContext('2d');
 
+		//start doing stuff once the tilesheet png loads
 		Game.tilesheet.onload = function(){
+			
+			//render out the whole tilesheet to the offscreen canvas
 			Game.tilesheetContext.drawImage(Game.tilesheet,0,0);
-			Game.getTiles();
+
+			//get all the tiles for the current viewport (default to 0,0)
+			Game._getTiles(0,0,function(){
+
+				//new tile data stored in nextTiles by default
+				//since this is the initial load w/ no transition, 
+				//copy them over to currentTiles instead of transitioning
+				Game._copyTileArray(function(){
+
+					//render out every tile in currentTiles
+					Game._renderAll();
+				});
+			});
 		};
 	},
 
-	getTiles: function() {
-		ss.rpc('multiplayer.getMapData',Game.masterX,Game.masterY,Game.masterX+30,Game.masterY+15,function(response){
-			Game.tiles = response;
-			Game.render();
+	_copyTileArray: function(callback){
+		for(var t=0;t<Game.nextTiles.length;t++){
+			Game.currentTiles[t] = Game.nextTiles[t];
+		}
+		callback();
+	},
+
+	_getTiles: function(x,y,callback) {
+		ss.rpc('multiplayer.getMapData',x,y,x+30,y+15,function(response){
+			Game.nextTiles = response;
+			callback();
 		});
 	},
-	render: function() {
-
-		for(var i=0;i<Game.tiles.length;i++){
-			//console.log(Game.tiles[i].background);
-			var index = Game.tiles[i].background-1;
+	
+	_renderTile: function(index, srcX, srcY, destX, destY) {
+		Game.backgroundContext.drawImage(
+			Game.tilesheet, 
+			srcX*Game.tileSize,
+			srcY*Game.tileSize,
+			Game.tileSize,
+			Game.tileSize,
+			destX*Game.tileSize,
+			destY*Game.tileSize,
+			Game.tileSize,
+			Game.tileSize
+		);
+	},
+	
+	_renderAll: function() {
+		for(var i=0;i<Game.currentTiles.length;i++){
+			//tilemap starts at 1 instead of 0
+			var index = Game.currentTiles[i].background-1;
 			var srcX = index % Game.tilesheetWidth;
 			var srcY = Math.floor(index / Game.tilesheetWidth);
 			var destX = i % Game.tilesWidth; 
 			var destY = Math.floor(i/Game.tilesWidth);
 
-			Game.backgroundContext.drawImage(
-				Game.tilesheet, 
-				srcX*Game.tileSize,
-				srcY*Game.tileSize,
-				Game.tileSize,
-				Game.tileSize,
-				destX*Game.tileSize,
-				destY*Game.tileSize,
-				Game.tileSize,
-				Game.tileSize
-				);
+			Game._renderTile(index, srcX, srcY, destX, destY);
 		}	
+	},
+
+	getNoGo: function(x,y) {
+		var i = y*Game.tilesWidth + (x%Game.tilesWidth);
+		var noGoVal = Game.currentTiles[i].nogo;
+		//console.log(noGoVal);
+		return noGoVal;
+	},
+
+	getWorldEdge: function(x,y){
+		var i = y*Game.tilesWidth + (x%Game.tilesWidth);
+		var edge = Game.currentTiles[i].isWorldEdge;
+		return edge;
+	},
+
+	beginTransition: function(x,y){
+		var isEdge = Game.getWorldEdge(x,y);
+
+		if(!isEdge){
+			if(x==0){
+				Game.nextX = Game.masterX-(Game.tilesWidth-1);
+				Game.stepX = -1;
+				Game.shiftArray = -1;
+			}
+			else if(x==Game.tilesWidth-1){
+				Game.nextX = Game.masterX+(Game.tilesWidth-1);
+				Game.stepX = 1;
+				Game.shiftArray = 1;
+			}
+			else if(y==0){
+				Game.nextY = Game.masterX-(Game.tilesHeight-1);
+				Game.stepY = -1;
+				Game.shiftArray = -totalTilesHeight;
+			}
+			else if(y==Game.tilesHeight-1){
+				Game.nextY = Game.masterX+(Game.tilesHeight-1);
+				Game.stepY = 1;
+				Game.shiftArray = totalTilesHeight;
+			}
+			Game._getTiles(Game.nextX,Game.nextY,function(){
+				Game._stepTransition();
+			});
+		}
+	},
+
+	_stepTransition: function(){
+		if(Game.masterX!=Game.nextX){
+			Game.masterX+=Game.stepX;
+			Game._updateAndDraw();
+		}
+		else if(Game.masterY!=Game.nextY){
+			Game.masterY+=Game.stepY;
+			Game._updateAndDraw();
+		}
+		else{
+			Game._endTransition();
+		}
+	},
+
+	_endTransition: function(){
+		inTransit = false;
+		console.log("done!!");
+	},
+
+	_updateAndDraw: function(){
+		console.log(Game.masterX+", "+Game.masterY);
+		// for(var i=0;i<currentTiles.length;i++){
+			
+		// }
+		requestAnimFrame( Game._stepTransition );
+    	//Game._renderAll();
 	}
 };
 
 $(document).ready(function(){
-		
-	Game.init();
-	$(".gameboard").click(function(e){
-		Game.getTiles();
-	});
-});
+	
+	
+	//returns local x,y grid data based on mouse location
+	getXYFromMouse = function(a,b,oa,ob,callback){
+		var x = a - oa;
+		var y = b - ob;
+		var snapX = Math.floor(x/32);
+		var snapY = Math.floor(y/32);
+		return callback(snapX,snapY);
+	};
 
-
-//******************** mouse interaction with grid 
-	// calculateMouse = function(a,b,oa,ob,callback){
-	// 	var x = a - oa;
-	// 	var y = b - ob;
-	// 	var snapX = Math.floor(x/32);
-	// 	var snapY = Math.floor(y/32);
-	// 	return callback(snapX,snapY);
-	// };
-	// $(".gameboard").mousemove(function(m) {
-	// 	calculateMouse(m.pageX,m.pageY,this.offsetLeft,this.offsetTop,function(x,y){
+	//change cursor on mouse move
+	$(".gameboard").mousemove(function(m) {
+		getXYFromMouse(m.pageX,m.pageY,this.offsetLeft,this.offsetTop,function(x,y){
 			
-	// 		var noGoValue = Game.getNoGo(x,y);
-	// 		Game.setCurrentTile(x,y);
-	// 		var tempColor;
-	// 		if(noGoValue){
-	// 			tempColor = 'red';
-	// 		}
-	// 		else{
-	// 			tempColor = 'white';
-	// 		}
-	// 		//change cursor style and location
-	// 		$(".cursor").css({
-	// 			'left': x*32,
-	// 			'top': y*32,
-	// 			'border-color': tempColor
-	// 		});
-	// 	});
- // 	});
+			var noGoValue = Game.getNoGo(x,y);
+			var tempColor = noGoValue ? 'red' : 'white';
+			
+			$(".cursor").css({
+				'left': x*32,
+				'top': y*32,
+				'border-color': tempColor,
+			});
+		});
+ 	});
 
- 	//******* moving player on click for now (only on edge of screen)
-	// $(".gameboard").click(function(m){
-	// 	if(Game.currentTile.nogo==false){
+ 	//figure out if we should transition (or do other stuff later)
+ 	$(".gameboard").click(function(m){
+ 		if(!inTransit){
+ 			inTransit = true;
+ 			getXYFromMouse(m.pageX,m.pageY,this.offsetLeft,this.offsetTop,function(x,y){
+ 				Game.beginTransition(x,y);
+ 			});
+ 		}
+ 	});
 
-	// 		//left
-	// 		if(Game.currentTile.x<1){
-	// 			//move bg
-	// 			$(".map").addClass("map-left");
-	// 			var newX = parseInt($(".map").css("background-position-x"))+896;
-	// 			var newY = parseInt($(".map").css("background-position-y"));
-	// 			$(".map-left").css({
-	// 				"background-position-x":newX+"px",
-	// 				"background-position-y":newY+"px"
-	// 			});
+ 	Game.init();
 
-	// 			//move grid
-
-	// 			$(".current").addClass("shiftRight");
-
-	// 			//new quad is -1
-	// 			Game.changeQuad(-1);
-	// 		}
-	// 		//right
-	// 		else if(Game.currentTile.x>28){
-	// 			$(".topRight").addClass("horizontalQuad").removeClass("cornerQuad");
-	// 			$(".middleRight").addClass("fullQuad").removeClass("verticalQuad");
-	// 			$(".bottomRight").addClass("horizontalQuad").removeClass("cornerQuad");
-				
-	// 			//move bg
-	// 			$(".map").addClass("map-right");
-	// 			var newX = parseInt($(".map").css("background-position-x"))-896;
-	// 			var newY = parseInt($(".map").css("background-position-y"));
-	// 			$(".map-right").css({
-	// 				"background-position-x":newX+"px",
-	// 				"background-position-y":newY+"px"
-	// 			});
-				
-	// 			//move grid
-	// 			$(".current").css("width","1856px");
-	// 			$(".current").addClass("shiftLeft");
-				
-	// 			//new quad is +1
-	// 			Game.changeQuad(1);
-	// 		}
-	// 		//up
-	// 		else if(Game.currentTile.y<1){
-
-	// 			$(".map").addClass("map-up");
-	// 			var newX = parseInt($(".map").css("background-position-x"));
-	// 			var newY = parseInt($(".map").css("background-position-y"))+416;
-	// 			$(".map-up").css({
-	// 				"background-position-x":newX+"px",
-	// 				"background-position-y":newY+"px"
-	// 			});
-
-	// 			//move grid
-	// 			$(".current").addClass("shiftUp");
-	// 			//new quad is -5
-	// 			Game.changeQuad(-5);
-	// 		}
-	// 		//down
-	// 		else if(Game.currentTile.y>13){
-	// 			$(".map").addClass("map-down");
-	// 			var newX = parseInt($(".map").css("background-position-x"));
-	// 			var newY = parseInt($(".map").css("background-position-y"))-416;
-	// 			$(".map-down").css({
-	// 				"background-position-x":newX+"px",
-	// 				"background-position-y":newY+"px"
-	// 			});
-	// 			//move grid
-	// 			$(".current").addClass("shiftDown");
-	// 			//new quad is +5
-	// 			Game.changeQuad(5);
-	// 		}
-	// 		//reset 
-	// 		$(".map").bind('transitionend webkitTransitionEnd', function() { 
-	// 			$(this).removeClass("map-left");
-	// 			$(this).removeClass("map-right");
-	// 			$(this).removeClass("map-up");
-	// 			$(this).removeClass("map-down");
-	// 			$("topLeft").remove();
-	// 			$(".current").css({
-	// 				"width": "960px"
-	// 				// "left": "0px"
-	// 			});
-	// 			//$(".current").removeClass("shiftLeft");
-	// 			$(".middleMiddle").css({
-	// 				"width":"32px",
-	// 				"left": "896px"
-	// 			});
-	// 			$(".cube").css("left","0px");
-
-	// 		});
-	// 	}
-	// 		Game.info();
-	// });
-	//********************mouse interaction end ******
+});
 
 
 angular.module('multiPlayer', ['ssAngular'])
