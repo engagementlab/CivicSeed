7565e915071edd905a94ef3668f49ef43804370b
American Yak <langbert@gmail.com>
2012-09-11 20:01:05 -0400

    development environment -- branching this site for other environments

--- environment.js
+++ environment.js
@@ -1,51 +1,29 @@
-// to run a specific environment, set the NODE_ENV variable on run:
-// also, to run a specific database, set the ALT_DB variable on run:
-// example: 'NODE_ENV=development ALT_DB=testing nodemon app.js'
-// production is default/fallback where nothing is set
-var nodeEnv = process.env.NODE_ENV || 'development',
-databaseEnv = process.env.ALT_DB || nodeEnv,
-redisPort,
-redisHost,
-databaseURL,
-siteUrl;
+/*
+ * ENVIRONMENT/GLOBAL VARIABLES for DEVELOPMENT/LOCAL CONFIGURATION
+ *
+ * to run a specific environment, set the NODE_ENV variable on run:
+ * also, to run a specific database, set the ALT_DB variable on run:
+ * example: 'NODE_ENV=development ALT_DB=testing nodemon app.js'
+ * production is default/fallback where nothing is set
+ *
+ */
 
-// configure database according to environment
-// the order of configuration here is important
-if(databaseEnv === 'production') {
-} else if(databaseEnv === 'staging') {
-	siteUrl = 'http://civicseed-test.nodejitsu.com/';
-	databaseURL = 'mongodb://root:root@ds033767.mongolab.com:33767/civicseeddev';
-	redisPort = 6379;
-	redisHost = 'localhost'; // ??????
-} else if(databaseEnv === 'testing') {
-	siteUrl = 'http://civicseed-test.nodejitsu.com/';
-	databaseURL = 'mongodb://root:root@ds033767.mongolab.com:33767/civicseeddev';
-	redisPort = 6379;
-	redisHost = 'localhost'; // ??????
-} else if(databaseEnv === 'development') {
-	siteUrl = 'http://localhost:3000/';
-	databaseURL = 'mongodb://localhost/civic_dev_db';
-	redisPort = 6379;
-	redisHost = 'localhost';
-} else {
-	console.log('  MONGODB AND/OR REDIS CONNECTION INFORMATION MISSING OR INACCURATE  '.red.inverse);
-}
+var nodeEnv = process.env.NODE_ENV || 'development';
 
-// environment/global variables
-var globals = module.exports = {
+var self = module.exports = {
 	app: {
 		name: 'Civic Seed',
 		nodeEnv: nodeEnv,
 		initialized: false,
-		siteUrl: siteUrl,
+		siteUrl: process.env.SITE_URL || 'http://localhost:3000/',
 	},
 	redis: {
-		port: redisPort,
-		host: redisHost,
+		port: process.env.REDIS_PORT || 6379,
+		host: process.env.REDIS_HOST || 'localhost',
 	},
 	database: {
-		environment: databaseEnv,
-		URL: databaseURL,
+		environment: process.env.ALT_ENV || nodeEnv,
+		URL: process.env.DATABASE_URL || 'mongodb://localhost/civic_dev_db',
 	},
 	map: {
 		mapTilesWidth: 142,
