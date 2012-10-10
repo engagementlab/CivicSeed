ab3aa77423d032946d4004f840803a55bc32aedf
American Yak <langbert@gmail.com>
2012-10-10 01:24:07 -0400

    Separated startup from invite loader.

--- controllers/admin/startup-control.js
+++ controllers/admin/startup-control.js
@@ -4,7 +4,6 @@ var dbActions = require(rootDir + '/modules/utils/databaseActions');
 var environment;
 var nodeEnv;
 var User;
-var Invitee;
 
 var handleError = function(message, err) {
 	if(err) {
@@ -28,26 +27,17 @@ var self = module.exports = {
 		environment = service.environment;
 		nodeEnv = environment.app.nodeEnv;
 		User = service.useModel('user').UserModel;
-		Invitee = service.useModel('user', 'preload').InviteeModel;
 
 		self.service = service;
 
 		app.get('/admin/startup', function(req, res) {
 
-			var consoleOutput;
-			Invitee.collection.distinct('sessionName', function(err, invitees) {
-				if(err) {
-					console.error('Could not find document: %s', err);
-				}
-				// console.log(invitees);
-				res.render('admin/startup.hbs', {
-					title: 'Startup',
-					bodyClass: 'admin startup',
-					nodeEnv: nodeEnv,
-					consoleOutput: consoleOutput,
-					message: 'Startup admin panel.',
-					invitees: invitees,
-				});
+			res.render('admin/startup.hbs', {
+				title: 'Startup',
+				bodyClass: 'admin startup',
+				nodeEnv: nodeEnv,
+				// consoleOutput: consoleOutput,
+				message: 'Startup admin panel.'
 			});
 
 		});
@@ -160,65 +150,6 @@ var self = module.exports = {
 		// 	}
 		// 	res.redirect('/login');
 		// });
-
-		// create a set of codes
-		app.get('/admin/action/create-invite-codes/:sessionName', function(req, res) {
-			var i;
-			var inviteeGroup = [];
-			var inviteeObject;
-			var sessionName = req.params.sessionName;
-			function createCode() {
-				var codeArray = [];
-				var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.:;{}[]|-=_+()&^%$#@!?~`ç√∫¥';
-				for(var i=0; i < 50; i++) {
-					codeArray[i] = charSet.charAt(Math.floor(Math.random() * charSet.length));
-				}
-				return codeArray.join('');
-			};
-
-			// TODO: add in emails...???
-			for(i = 0; i < 30; i++) {
-				inviteeObject = {};
-				inviteeObject.sessionName = sessionName;
-				// inviteeObject.email = '???@???.???';
-				inviteeObject.accepted = false;
-				inviteeObject.code = createCode();
-				inviteeGroup.push(inviteeObject);
-			}
-
-			// console.log(inviteeGroup);
-
-			Invitee.create(inviteeGroup, function(err) {
-				if(err) {
-					console.error('  Could not create documents: %s  '.yellow.inverse, err);
-					res.send('Error creating invite codes...');
-				} else {
-
-					Invitee.find({ sessionName: sessionName }, 'code', function (err, codes) {
-						var length = codes.length;
-						var i;
-						var codesArray = [];
-						for(i = 0; i < length; i++) {
-							codesArray.push(codes[i].code);
-						}
-						// console.log(codesArray);
-						console.log('CS: '.blue + 'Invite codes created and saved to database: '.green);
-						res.send(codesArray);
-					});
-
-				}
-			});
-
-		});
-
-		// recreate/overwrite a set of existing session codes
-		app.get('/admin/action/recreate-invite-codes/:session', function(req, res) {
-			// res.render('admin/admin.hbs', {
-			// 	title: 'Admin',
-			// 	bodyClass: 'admin',
-			// 	message: req.params.session,
-			// });
-		});
 
 	}
 
