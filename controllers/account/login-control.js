fc9fc54690c4e52fe44fabb8df3bfae676e2207c
American Yak <langbert@gmail.com>
2012-10-30 22:44:49 -0400

    Semi-working login, in case I need to roll back.

--- controllers/account/login-control.js
+++ controllers/account/login-control.js
@@ -7,115 +7,77 @@ var self = module.exports = {
     init: function (app, service, hbs) {
 
         //  var accountMiddleware = service.useModule('middleware/account');
-        var users = service.useModel('user');
+        var users = service.useModel('user').UserModel;
 
-        //  // app.get('/', accountMiddleware.requireRole('user'), function(req, res){
-        //  //  res.render('index', { title: "Index" });
-        //  // });
+		// see home-control for app.get('/' ...
+		app.post('/',
+			passport.authenticate('local', { failureRedirect: '/', failureFlash: true }),
+			function(req, res) {
+				console.log('CS: user has logged in successfully.');
+				res.redirect('/game');
+			}
+		);
 
-        passport.use(new LocalStrategy(
-            function(username, password, done) {
-                console.log(username+": "+password);
-                //check DB for user
-                checkLoginInfo(username,password,function(err,user){
-                    if(err){
-                        console.log("the error: "+err);
-                        return done(null,false,{message:"incorrect informacion"});
+		passport.use(new LocalStrategy(
+			function(username, password, done) {
+				checkLoginInfo(username,password,function(err,user){
+					if(err) {
+						console.log('the error: ' + err);
+						return done(null, false, {message: 'incorrect informacion'});
+					}
+					else{
+						return done(null,user);
+					}
+				});
+			}
+		));
+
+		passport.serializeUser(function(user, done) {
+			
+			done(null, user.id);
+		});
+
+		passport.deserializeUser(function(user, done) {
+			done(err, user.id);
+		});
+
+		checkLoginInfo = function(email, pass, callback) {
+			console.log(email.magenta.inverse);
+			users.findOne({email: email} , function(err, user) {
+				if(!user) {
+					return callback('you don\'t belong here.',null);
+				}
+				else{
+					// var hashedPassword = user.password;
+					// if(hash.verify(pass, hashedPassword)){
+					//     return callback(null,user);
+					// }
+                    if(user.password == pass) {
+                    	return callback(null,user);
                     }
                     else{
-                        return done(null,user);
-                    }
-                });
-            }    
-            
-            // //hard test to see if passport is working
-            //     if(username=="gnome"){
-            //         if(password=="chomsky"){
-            //             return done(null,true);
-            //         }
-            //         else{
-            //             return done(null,false,{message:'wrong password'});
-            //         }
-            //     }
-            //     else{
-            //         return done(null,false,{message: 'wrong user'});
-            //     }
-            // }
-        ));
-
-    // Passport session setup.
-    //   To support persistent login sessions, Passport needs to be able to
-    //   serialize users into and deserialize users out of the session.  Typically,
-    //   this will be as simple as storing the user ID when serializing, and finding
-    //   the user by ID when deserializing.
-
-
-    //THIS STUFF is automatically called when passport authenticates
-        passport.serializeUser(function(user, done) {
-        //what is this anyway??
-        // console.log("ser, "+user.email);
-        // done(null, user.email);
-           done(null,user);
-        });
-
-        passport.deserializeUser(function(user,done) {
-        //what is this anyway?
-        // console.log("derr, "+email);
-        // db.findByEmail(email, function (err, user) {
-        //     done(err, user);
-        // });
-            done(err,user);
-        });
-
-        checkLoginInfo = function(name,pass,callback){
-            users.findOne({name:name},function(err,user){
-                if(!user){
-                    return callback("you don't belong here.",null);
-                }
-                else{
-                    // var hashedPassword = user.password;
-                    // if(hash.verify(pass, hashedPassword)){
-                    //     return callback(null,user);
-                    // }
-                    if(user.password==pass){
-                        return callback(null,user);
-                    }
-                    else{
-                        return callback("wrong! try again.",null);
+                    	return callback('wrong! try again.',null);
                     }   
                 }
             });
-        };
+		};
 
 
 
-        app.get('/login',  function(req, res) {
-            res.render('login.hbs', {
-                title: 'Login',
-                bodyClass: 'login',
-                message: req.flash('error')
-            });
+		app.get('/login',  function(req, res) {
+			res.render('login.hbs', {
+				title: 'Login',
+				bodyClass: 'login',
+				message: req.flash('error')
+			});
                 // if(req.user==undefined){
                 //  res.redirect('/login');
                 // }
                 // else{
                 //  res.redirect('/profile');
                 // }
-        });
-        app.post('/', 
-          passport.authenticate('local', { failureRedirect: '/', failureFlash: true }),
-          function(req, res) {
-            console.log("yaaay");
-            res.redirect('/game');
-          });
-        // app.get('/signup/:email/:random',routes.signup);
-        // app.post('/signup',routes.newUser);
+            });
 
-        // app.get('/forgot',routes.forgot);
-        // app.post('/forgot',routes.forgotSend);
-
-
-
-    }
+	}
 
 };
---
+++ server/middleware/account/user.js
@@ -0,0 +1,27 @@
+// Only let a request through if the session has been authenticated
+exports.authenticated = function(framework) {
+	return function(req, res, next) {
+		if(req.session && (req.session.userId != null)) {
+			console.log('GREEN LIGHT'.green.inverse);
+			return next();
+		} else {
+			if(framework === 'express') {
+				if(req.url.match(/^\/admin/g)) {
+					console.log('Not authenticated . . . rerouting . . . '.yellow.inverse);
+					return res.redirect('/');
+				}
+				return next();
+			} else {
+				return res('Non admitte!');
+			}
+		}
+	};
+};
+
+// EXAMPLE
+// exports.log = function() {
+// 	return function(req, res, next) {
+// 		console.log('%s %s'.magenta.inverse, req.method, req.url);
+// 		next();
+// 	};
+// };
