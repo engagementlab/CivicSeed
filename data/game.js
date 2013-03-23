fb4116a285b4c5ae5aa2be8320bf65c01a6dbee7
russell goldenberg <russellgoldenberg@gmail.com>
2013-03-22 21:53:51 -0400

    slight changes for demo mode

--- data/game.js
+++ data/game.js
@@ -1,4 +1,4 @@
-module.exports.global = {
+module.exports.global = [{
 	players: 0,
 	tilesColored: 0,
 	seedsDropped: 0,
@@ -11,4 +11,17 @@ module.exports.global = {
 	resourceCount: [10, 10, 10, 10],
 	instanceName: 'awesome',
 	resourceResponses: []
-};
+}, {
+	players: 0,
+	tilesColored: 0,
+	seedsDropped: 0,
+	seedsDroppedGoal: 3200,
+	active: true,
+	bossModeUnlocked: false,
+	levelQuestion: ['What is your background?', 'Where do you like to work?', 'What time is it?', 'When are you done?'],
+	leaderboard: [],
+	levelNames: ['Level 1: Who are you?', 'Level 2: Where are you?', 'Level 3: When are you?', 'Level 4: Why are you?', 'Level 5: What are you?'],
+	resourceCount: [10, 10, 10, 10],
+	instanceName: 'demo-game',
+	resourceResponses: []
+}];
