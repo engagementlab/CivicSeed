bec7700335c3163a17173ff17875e47f83d2e06d
American Yak <langbert@gmail.com>
2013-08-29 16:01:17 -0400

    Test mongo connection in prod. Moved boot file.

---
+++ test/test-connect-mongo.js
@@ -0,0 +1,17 @@
+var colors = require('colors');
+var mongoose = require('mongoose');
+var envConnectionString = process.env.MONGO_CON;
+var connectionString = envConnectionString ? envConnectionString : 'mongodb://ip-10-202-153-133.ec2.internal/civicseed_testing';
+var db = mongoose.createConnection(connectionString);
+
+console.log('Testing mongo startup...');
+
+db.on('error', console.error.bind(console, ' CONNECTION ERROR: '.red));
+
+db.once('open', function (one, two, three, four, five) {
+	console.log('MongoDB connection opened...'.green);
+	mongoose.connection.close(function() {
+		console.log('MongoDB connection closed...'.green);
+		process.exit();
+	});
+});
