40c535184f6e4f1d00f4797becd168acf7905144
Lou Huang <lou@louhuang.com>
2014-12-05 11:02:45 -0500

    HTML formatting tweaks to resource articles. Formatting npc.js

--- client/code/game/game.npc.js
+++ client/code/game/game.npc.js
@@ -177,302 +177,300 @@ Npc.prototype.getSmalltalk = function () {
 }
 
 
-var $npc = $game.$npc = {
+var self = $game.$npc = (function () {
 
-  ready:      false,
-  hideTimer:  null,
+  // PRIVATE
+  var _npc = {
 
-  //pull all npc info from the DB
-  init: function (callback) {
-    //load all the npc info from the DB store it in an array
-    //where the index is the id of the npc / mapIndex
-    ss.rpc('game.npc.getNpcs', function (response) {
-      $.each(response, function (key, npc) {
-        _npc.add(npc)
-      })
-      $npc.ready = true
-      callback()
-    })
-  },
+    data: {},
 
-  resetInit: function () {
-    $npc.ready      = false
-    $npc.hideTimer  = null
-  },
+    // Add an npc to the data object
+    add: function (npcData) {
+      var npc = new Npc(npcData)
 
-  // Update all npcs (for movement and rendering)
-  update: function () {
-    $.each(_npc.data, function (key, npc) {
-      npc.update()
-    })
-  },
+      npc.setRenderInfo()
+      _npc.data[npc.id] = npc
+    },
 
-  // Clear all npcs to draw fresh
-  clear: function () {
-    $.each(_npc.data, function (key, npc) {
-      npc.clear()
-    })
-  },
+    //choose prompt based on PLAYERs memory of interaction
+    //there are 3 prompts (0: fresh visit, 1: visited, wrong answer, 2: already answered
+    createPrompt: function (npc) {
+      var promptIndex = $game.$player.getPrompt(npc.resource.id),
+          dialogue    = npc.dialog.prompts[promptIndex]
 
-  //get render info for all npcs to draw them
-  getRenderInfo: function () {
-    var allRenderInfo = []
-    if ((!$game.bossModeUnlocked && $game.$player.currentLevel > 3) || $game.$player.currentLevel <= 3) {
-      $.each(_npc.data, function (key, npc) {
-        var info = npc.getRenderInfo()
-        if (info) {
-          allRenderInfo.push(info)
-        }
+      if (promptIndex === 2) {
+        dialogue += ' Want to view again?'
+      }
+
+      self.showSpeechBubble(npc.name, dialogue, function () {
+        $game.$resources.showResource(npc.resource.id)
       })
     }
-    return allRenderInfo
-  },
 
-  // Determine NPC content to display when clicked
-  activate: function (npcId) {
-    var npc           = $npc.get(npcId),
-        botanistState = $game.$botanist.getState()
-
-    // Once activated, reset global NPC state
-    // TODO: This should be deprecated eventually
-    $game.$player.npcOnDeck = false
-
-    // NPC interaction to display if the player has not finished speaking with Botanist
-    // (1) If the player attempts to roam the world before completing the tutorial
-    if ($game.flags.check('first-time') === true) {
-      $npc.showSpeechBubble(npc.name, 'You should really see the Botanist before exploring the world.')
-    } else if (botanistState < 2 || botanistState > 3) {
-      // (2) If the player attempts to roam the world before the Botanist is done talking
-      $npc.showSpeechBubble(npc.name, 'The Botanist still has more to tell you! Head back to The Botanist’s Garden to hear the rest.')
-    } else if (npc.isHolding && $game.$player.getLevel() >= npc.getLevel()) {
-      // If resource is available for the player
-      // Check if NPC's availability depends on player talking to a different NPC
-      if (npc.isLocked()) {
-        var dialogue = 'Before I help you out, you need to go see ' + $npc.get(npc.dependsOn).name + '. Come back when you have their resource.'
-        $npc.showSpeechBubble(npc.name, dialogue)
-      } else {
-        _npc.createPrompt(npc)
-      }
-    } else {
-      // If no resource is available for the player, make small talk instead.
-      $npc.showSpeechBubble(npc.name, npc.getSmalltalk())
-    }
-  },
-
-  // Show an NPC's speech bubble
-  showSpeechBubble: function (speaker, messages, prompt, callback) {
-    var el            = document.getElementById('speech-bubble'),
-        $el           = $(el),
-        hasPrompt     = false,
-        isMultiline   = false,
-        text          = null
-
-    // If a speech bubble is currently open, just hide it quickly so that text change can happen
-    if ($el.is(':visible')) {
-      el.style.display = 'none'
-    }
-
-    // Set global chat state
-    $game.flags.set('npc-chatting')
-
-    // Play sound effect
-    $game.$audio.playTriggerFx('npcBubble')
-
-    // Set up message
-    $el.find('.speaker').text(speaker)
-
-    // Clear any residue of interaction detritus
-    $el.find('.dialog').removeClass('fit')
-    $el.find('button').hide()
-
-    if (_.isArray(messages)) {
-      // An array of strings is acceptable for messages.
-      // This would create 'next' buttons until the full array of messages have been displayed.
-      // If a prompt is provided, the prompt will always be on the last message.
-      // Callbacks are not performed until the player clicks 'Close' of the speech bubble.
-      // Sometimes it's useful to provide a single-item array if you want a user to acknowledge
-      // a speech bubble and the game to provide additional actions.
-      isMultiline = true
-      _showMultiline(0)
-    } else {
-      // Assume that messages is a string.
-      text = messages
-      $el.find('.message').text(text)
-    }
-
-    // If it has a prompt, set up prompt.
-    if (typeof prompt === 'function' && isMultiline === false) {
-      hasPrompt = true
-      _setupPrompt()
-    }
-
-    // If neither array nor prompt
-    if (!isMultiline && !hasPrompt) {
-      // Set up callback function
-      if (typeof callback === 'function') _storeCallback()
-    }
-
-    // Display the speech bubble
-    $el.fadeIn(300, function () {
-      // If no prompt, the dialog box should fade on its own after some time.
-      // The timer is set by the length of the message, but no less than 4 seconds at minimum.
-      if (!isMultiline && !hasPrompt) {
-        var hideTimer = text.length * 50
-        if (hideTimer < 4000) hideTimer = 4000
-        $npc.hideTimer = setTimeout($npc.hideSpeechBubble, hideTimer)
-      }
-    })
-
-    // Utility functions for showSpeechBubble()
-    function _showMultiline (index) {
-      $el.find('.dialog').addClass('fit')
-
-      text = messages[index]
-      $el.find('.message').text(text)
-
-      if (index < messages.length - 1) {
-        // Intermediary messages
-        $el.find('.next-button').off('click').on('click', function (e) {
-          e.stopImmediatePropagation()
-          _showMultiline(index + 1)
-        }).show()
-      } else {
-        // Last message
-        $el.find('.next-button').off('click').hide()
-        if (hasPrompt === true) {
-          _setupPrompt()
-        } else {
-          $el.find('.close-button').on('click', function (e) {
-            e.stopImmediatePropagation()
-            $npc.hideSpeechBubble(callback)
-          }).show()
-        }
-      }
-    }
-
-    function _setupPrompt () {
-      $el.find('.dialog').addClass('fit')
-
-      // prompt is a callback function that is executed when player clicks the Yes button.
-      // Currently assuming that all prompt responses automatically closes the speech bubble
-      // rather than lead to next line of conversation.
-      $el.find('.yes-button').on('click', function (e) {
-        e.stopImmediatePropagation()
-        $npc.hideSpeechBubble(prompt)
-      }).show()
-
-      // Close prompt
-      $el.find('.no-button').on('click', function (e) {
-        e.stopImmediatePropagation()
-        $npc.hideSpeechBubble(callback)
-      }).show()
-    }
-
-    function _storeCallback () {
-      // Binds callback to a hidden button element so that it is called on hide
-      var button = document.createElement('button')
-      button.id = 'callback-button'
-      button.addEventListener('click', function _onClose (e) {
-        e.preventDefault()
-        callback()
-        button.removeEventListener('click', _onClose)
-      })
-
-      el.querySelector('.buttons').appendChild(button)
-    }
-
-  },
-
-  // Hide an NPC's chat bubble
-  hideSpeechBubble: function (callback) {
-    clearTimeout($game.$npc.hideTimer)
-    var $el = $('#speech-bubble')
-
-    $game.flags.unset('npc-chatting')
-    $el.find('.next-button').off('click')
-    $el.find('.close-button').off('click')
-    $el.find('.yes-button').off('click')
-    $el.find('.no-button').off('click')
-
-    $el.fadeOut(300, function () {
-      // Execute a callback function passed to this method
-      if (typeof callback === 'function') callback()
-
-      // Execute a callback function bound to the speech bubble DOM
-      var $storedCallback = $el.find('.callback-button')
-      if ($storedCallback.length > 0) {
-        $storedCallback.triggerHandler('click')
-        $storedCallback.remove()
-      }
-    })
-  },
-
-  // Set the current npc to specific one so we can operate on it in the near future
-  select: function (npcId) {
-    // Pass the selected NPC id to $player to trigger the selected NPC
-    $game.$player.npcOnDeck = npcId
-  },
-
-  // Get an NPC by its ID
-  get: function (npcId) {
-    return _npc.data[npcId]
-  },
-
-  //get all npc data
-  getNpcData: function () {
-    return _npc.data
-  },
-
-  findNpcByResourceId: function (id) {
-    // Get NPC data given its resource id
-    var result = _.filter(_npc.data, function (npc) {
-      return npc.resource.id == id
-    })
-    return result[0]
-  },
-
-  getOnScreenNpcs: function () {
-    var onScreenNpcs = []
-    $.each(_npc.data, function (key, npc) {
-      if (npc.onScreen) {
-        onScreenNpcs.push(npc.id)
-      }
-    })
-    return onScreenNpcs
   }
-}
 
-/**
-  *
-  *  PRIVATE FUNCTIONS
-  *
- **/
+  return {
+    ready:      false,
+    hideTimer:  null,
 
-var _npc = {
+    //pull all npc info from the DB
+    init: function (callback) {
+      //load all the npc info from the DB store it in an array
+      //where the index is the id of the npc / mapIndex
+      ss.rpc('game.npc.getNpcs', function (response) {
+        $.each(response, function (key, npc) {
+          _npc.add(npc)
+        })
+        self.ready = true
+        callback()
+      })
+    },
 
-  data: {},
+    resetInit: function () {
+      self.ready      = false
+      self.hideTimer  = null
+    },
 
-  // Add an npc to the data object
-  add: function (npcData) {
-    var npc = new Npc(npcData)
+    // Update all npcs (for movement and rendering)
+    update: function () {
+      $.each(_npc.data, function (key, npc) {
+        npc.update()
+      })
+    },
 
-    npc.setRenderInfo()
-    _npc.data[npc.id] = npc
-  },
+    // Clear all npcs to draw fresh
+    clear: function () {
+      $.each(_npc.data, function (key, npc) {
+        npc.clear()
+      })
+    },
 
-  //choose prompt based on PLAYERs memory of interaction
-  //there are 3 prompts (0: fresh visit, 1: visited, wrong answer, 2: already answered
-  createPrompt: function (npc) {
-    var promptIndex = $game.$player.getPrompt(npc.resource.id),
-        dialogue    = npc.dialog.prompts[promptIndex]
+    //get render info for all npcs to draw them
+    getRenderInfo: function () {
+      var allRenderInfo = []
+      if ((!$game.bossModeUnlocked && $game.$player.currentLevel > 3) || $game.$player.currentLevel <= 3) {
+        $.each(_npc.data, function (key, npc) {
+          var info = npc.getRenderInfo()
+          if (info) {
+            allRenderInfo.push(info)
+          }
+        })
+      }
+      return allRenderInfo
+    },
 
-    if (promptIndex === 2) {
-      dialogue += ' Want to view again?'
+    // Determine NPC content to display when clicked
+    activate: function (npcId) {
+      var npc           = self.get(npcId),
+          botanistState = $game.$botanist.getState()
+
+      // Once activated, reset global NPC state
+      // TODO: This should be deprecated eventually
+      $game.$player.npcOnDeck = false
+
+      // NPC interaction to display if the player has not finished speaking with Botanist
+      // (1) If the player attempts to roam the world before completing the tutorial
+      if ($game.flags.check('first-time') === true) {
+        self.showSpeechBubble(npc.name, 'You should really see the Botanist before exploring the world.')
+      } else if (botanistState < 2 || botanistState > 3) {
+        // (2) If the player attempts to roam the world before the Botanist is done talking
+        self.showSpeechBubble(npc.name, 'The Botanist still has more to tell you! Head back to The Botanist’s Garden to hear the rest.')
+      } else if (npc.isHolding && $game.$player.getLevel() >= npc.getLevel()) {
+        // If resource is available for the player
+        // Check if NPC's availability depends on player talking to a different NPC
+        if (npc.isLocked()) {
+          var dialogue = 'Before I help you out, you need to go see ' + self.get(npc.dependsOn).name + '. Come back when you have their resource.'
+          self.showSpeechBubble(npc.name, dialogue)
+        } else {
+          _npc.createPrompt(npc)
+        }
+      } else {
+        // If no resource is available for the player, make small talk instead.
+        self.showSpeechBubble(npc.name, npc.getSmalltalk())
+      }
+    },
+
+    // Show an NPC's speech bubble
+    showSpeechBubble: function (speaker, messages, prompt, callback) {
+      var el            = document.getElementById('speech-bubble'),
+          $el           = $(el),
+          hasPrompt     = false,
+          isMultiline   = false,
+          text          = null
+
+      // If a speech bubble is currently open, just hide it quickly so that text change can happen
+      if ($el.is(':visible')) {
+        el.style.display = 'none'
+      }
+
+      // Set global chat state
+      $game.flags.set('npc-chatting')
+
+      // Play sound effect
+      $game.$audio.playTriggerFx('npcBubble')
+
+      // Set up message
+      $el.find('.speaker').text(speaker)
+
+      // Clear any residue of interaction detritus
+      $el.find('.dialog').removeClass('fit')
+      $el.find('button').hide()
+
+      if (_.isArray(messages)) {
+        // An array of strings is acceptable for messages.
+        // This would create 'next' buttons until the full array of messages have been displayed.
+        // If a prompt is provided, the prompt will always be on the last message.
+        // Callbacks are not performed until the player clicks 'Close' of the speech bubble.
+        // Sometimes it's useful to provide a single-item array if you want a user to acknowledge
+        // a speech bubble and the game to provide additional actions.
+        isMultiline = true
+        _showMultiline(0)
+      } else {
+        // Assume that messages is a string.
+        text = messages
+        $el.find('.message').text(text)
+      }
+
+      // If it has a prompt, set up prompt.
+      if (typeof prompt === 'function' && isMultiline === false) {
+        hasPrompt = true
+        _setupPrompt()
+      }
+
+      // If neither array nor prompt
+      if (!isMultiline && !hasPrompt) {
+        // Set up callback function
+        if (typeof callback === 'function') _storeCallback()
+      }
+
+      // Display the speech bubble
+      $el.fadeIn(300, function () {
+        // If no prompt, the dialog box should fade on its own after some time.
+        // The timer is set by the length of the message, but no less than 4 seconds at minimum.
+        if (!isMultiline && !hasPrompt) {
+          var hideTimer = text.length * 50
+          if (hideTimer < 4000) hideTimer = 4000
+          self.hideTimer = setTimeout(self.hideSpeechBubble, hideTimer)
+        }
+      })
+
+      // Utility functions for showSpeechBubble()
+      function _showMultiline (index) {
+        $el.find('.dialog').addClass('fit')
+
+        text = messages[index]
+        $el.find('.message').text(text)
+
+        if (index < messages.length - 1) {
+          // Intermediary messages
+          $el.find('.next-button').off('click').on('click', function (e) {
+            e.stopImmediatePropagation()
+            _showMultiline(index + 1)
+          }).show()
+        } else {
+          // Last message
+          $el.find('.next-button').off('click').hide()
+          if (hasPrompt === true) {
+            _setupPrompt()
+          } else {
+            $el.find('.close-button').on('click', function (e) {
+              e.stopImmediatePropagation()
+              self.hideSpeechBubble(callback)
+            }).show()
+          }
+        }
+      }
+
+      function _setupPrompt () {
+        $el.find('.dialog').addClass('fit')
+
+        // prompt is a callback function that is executed when player clicks the Yes button.
+        // Currently assuming that all prompt responses automatically closes the speech bubble
+        // rather than lead to next line of conversation.
+        $el.find('.yes-button').on('click', function (e) {
+          e.stopImmediatePropagation()
+          self.hideSpeechBubble(prompt)
+        }).show()
+
+        // Close prompt
+        $el.find('.no-button').on('click', function (e) {
+          e.stopImmediatePropagation()
+          self.hideSpeechBubble(callback)
+        }).show()
+      }
+
+      function _storeCallback () {
+        // Binds callback to a hidden button element so that it is called on hide
+        var button = document.createElement('button')
+        button.id = 'callback-button'
+        button.addEventListener('click', function _onClose (e) {
+          e.preventDefault()
+          callback()
+          button.removeEventListener('click', _onClose)
+        })
+
+        el.querySelector('.buttons').appendChild(button)
+      }
+
+    },
+
+    // Hide an NPC's chat bubble
+    hideSpeechBubble: function (callback) {
+      clearTimeout(self.hideTimer)
+      var $el = $('#speech-bubble')
+
+      $game.flags.unset('npc-chatting')
+      $el.find('.next-button').off('click')
+      $el.find('.close-button').off('click')
+      $el.find('.yes-button').off('click')
+      $el.find('.no-button').off('click')
+
+      $el.fadeOut(300, function () {
+        // Execute a callback function passed to this method
+        if (typeof callback === 'function') callback()
+
+        // Execute a callback function bound to the speech bubble DOM
+        var $storedCallback = $el.find('.callback-button')
+        if ($storedCallback.length > 0) {
+          $storedCallback.triggerHandler('click')
+          $storedCallback.remove()
+        }
+      })
+    },
+
+    // Set the current npc to specific one so we can operate on it in the near future
+    select: function (npcId) {
+      // Pass the selected NPC id to $player to trigger the selected NPC
+      $game.$player.npcOnDeck = npcId
+    },
+
+    // Get an NPC by its ID
+    get: function (npcId) {
+      return _npc.data[npcId]
+    },
+
+    //get all npc data
+    getNpcData: function () {
+      return _npc.data
+    },
+
+    findNpcByResourceId: function (id) {
+      // Get NPC data given its resource id
+      var result = _.filter(_npc.data, function (npc) {
+        return npc.resource.id == id
+      })
+      return result[0]
+    },
+
+    getOnScreenNpcs: function () {
+      var onScreenNpcs = []
+      $.each(_npc.data, function (key, npc) {
+        if (npc.onScreen) {
+          onScreenNpcs.push(npc.id)
+        }
+      })
+      return onScreenNpcs
     }
+  }
 
-    $npc.showSpeechBubble(npc.name, dialogue, function () {
-      $game.$resources.showResource(npc.resource.id)
-    })
-  },
-
-}
+})()
